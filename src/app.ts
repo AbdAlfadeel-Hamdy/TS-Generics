343145e91d0914e25a3d9740bb3cdb7fc291c769
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-14 10:58:55 +0200

    generic function

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,35 @@
 // Code goes here!
+// const names: Array<string> = []; // === string[]
+
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
+
+// promise.then((data) => {
+//   data.toFixed();
+// });
+
+//////////////////////// GENERIC FUNCTION //////////////////////////////
+//////////////////////// CONSTRAINTS //////////////////////////////
+
+const merge = <T extends object, U extends object>(objA: T, objB: U) => {
+  return Object.assign(objA, objB);
+};
+
+const mergedObj = merge({ name: "Gonzalo" }, { age: 22 });
+
+interface Lengthy {
+  length: number;
+}
+
+const countAndDescribe = <T extends Lengthy>(element: T): [T, string] => {
+  let text = "Got no value!";
+  if (element.length === 0) text = "Got 1 element.";
+  if (element.length > 1) text = `Got ${element.length} elements.`;
+  return [element, text];
+};
+
+console.log(countAndDescribe("Gonzalo"));
+console.log(countAndDescribe([1, 2, 3]));
